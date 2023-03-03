99cff3ddf5f0ce5fef7403e2f40bb2a16735a36b
KESAR ASSUDANI <110725209+kesar47@users.noreply.github.com>
2023-03-03 11:37:09 +0530

    Add files via upload

---
+++ img/serviceworker.js
@@ -0,0 +1,80 @@
+// var staticCacheName = "pwa";
+// self.addEventListener("install", function (e) {
+// e.waitUntil(
+// caches.open(staticCacheName).then(function (cache) {
+// return cache.addAll(["/"]);
+// })
+// );
+// });
+// self.addEventListener("fetch", function (event) {
+// console.log(event.request.url);
+// event.respondWith(
+// caches.match(event.request).then(function (response) {
+// return response || fetch(event.request);
+// })
+// );});
+
+var staticCacheName = "pwa";
+self.addEventListener("fetch", function (event) {
+console.log("Service Worker Has been Enabled");
+});
+self.addEventListener("install", function (e) {
+e.waitUntil(
+caches.open(staticCacheName).then(function (cache) {
+return cache.addAll(["/img/cat-4.jpg"]);
+})
+);
+});
+self.addEventListener("install", function (e) {
+e.waitUntil(
+caches.open(staticCacheName).then(function (cache) {
+return cache.addAll(["/css/style.css"]);
+})
+);
+});
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll(["/img/vendor-1.jpg"]);
+    })
+    );
+    });
+    self.addEventListener("install", function (e) {
+    e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll(["/contact.html"]);
+    })
+    );
+    });
+    self.addEventListener("install", function (e) {
+    e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll(["/index.html"]);
+    })
+    );
+    });
+    self.addEventListener("install", function (e) {
+    e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll(["/js/main.js"]);
+    })
+    );
+    });
+    self.addEventListener("install", function (e) {
+    e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll(["/img/vendor-2.jpg"]);
+    })
+    );
+    });
+    self.addEventListener("fetch", function (event) {
+    console.log(event.request.url);
+    event.respondWith(
+    caches.match(event.request).then(function (response) {
+    return response || fetch(event.request);
+    })
+    );
+});
+self.addEventListener("fetch", function (event) {
+console.log("Service Worker Has been Enabled");
+});
